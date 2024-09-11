5c7a1f697e8db596a60f75145197a68c766c904e
Vishal <vishal198shetty@gmail.com>
2024-09-11 16:19:06 +0530

    updates

--- src/Prizes.jsx
+++ src/Prizes.jsx
@@ -4,22 +4,22 @@ import first from './first-icon.webp'
 import second from './second-icon.webp'
 const Prizes = () => {
   return (<>
-  <h1 className='marginplease' id='prizehead' > Prizes</h1>
-  <h1 className='marginplease' id='prizehead' style={{fontSize:'2rem'}}> Inter College Coding Events</h1>
+  <h1 className='marginplease fsize' id='prizehead' > Prizes</h1>
+  <h1 className='marginplease fsize' id='prizehead2' > Inter College Coding Events</h1>
     <div id='prizes' className='subcomponent'>
-<div style={{width:'80vw',fontSize:'2rem',textAlign:'center'}}>
-  <table >
+<div  style={{width:'80vw',fontSize:'2rem',textAlign:'center'}}>
+  <table id='achtable'>
     <tr>
-    <td className='td1'><img src={first} style={{width:'5rem'}} alt="" /></td>
+    <td className='td1'><img src={first}  alt="" /></td>
     <td className='td2'>Medha Fest 23-SDIT College Kenjar</td>
     </tr>
   <tr className='tr1'>
-    <td className='td1'><img src={second} style={{width:'5rem'}} alt="" />
+    <td className='td1'><img src={second}  alt="" />
   </td>
   <td className='td2'>Razzmatazz Fest 24-Dr NSAM College Nitte</td>
   </tr>
   <tr className='tr1'>
-    <td className='td1'><img src={second} style={{width:'5rem'}} alt="" />
+    <td className='td1'><img src={second}  alt="" />
   </td>
   <td className='td2'>Samagra Fest 24-Sharada College Thallapady</td>
   </tr>
@@ -28,7 +28,7 @@ const Prizes = () => {
 
 
 </div>
-    <img src={prize} style={{width:'495px',marginTop:'-60px'}} alt="" /></div>
+    <img src={prize} className='hidden' style={{width:'495px',marginTop:'-60px'}} alt="" /></div>
     </>
   )
 }
