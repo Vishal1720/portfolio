ce9b694b86c82eb02b2eb75054c84c2c4f667413
Vishal <vishal198shetty@gmail.com>
2024-09-12 15:06:29 +0530

    modified:   src/App.css 	modified:   src/Nav.jsx

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -10,12 +10,14 @@ import prize from './prize-icon.png'
 function Nav(){return(
     <>
     <div id='nav' className='center'>
-    <h1 className='center'>Vishal Shetty</h1>
+    <h1 className='center' id='myname'>Vishal Shetty</h1>
     <img  src={pfp} id='pfp'></img><br></br>
+    <div className="links">
     <a href='https://www.linkedin.com/in/vishalshetty17/' target='_blank'><img src={linkedin} id='linkedin'></img></a>
     <a href="https://github.com/Vishal1720" target='_blank'><img src={github} id='github'></img></a>
     <a href="mailto:vishal198shetty@gmail.com" target='_blank'><img src={gmail} id='gmail'></img></a>
     <a href="https://codeflix2u.blogspot.com/2023/06/basic-steps.html" target='_blank'><img src={blogger} id='blogger'></img></a><br></br>
+    </div>
     <div className="linksContainer">
     
     <img id='homeicon' src={intro}></img>
